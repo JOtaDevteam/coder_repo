eee4bbc9534bb62dc26181da3c3845f615696038
Jimmy Code <jimmyornay24@gmail.com>
2022-05-17 21:14:51 -0300

    agrego el main.py a mi repo

---
+++ main.py
@@ -0,0 +1,10 @@
+from paquete_matematica.algebra import *
+from paquete_matematica import ecuaciones
+
+print("hola")
+result_sum = sumar(1, 3)
+result_rest = restar(1,2)
+result_ecua = sumar(4, 2)
+
+print (result_rest)
+
